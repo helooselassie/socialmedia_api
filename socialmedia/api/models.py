b1d7d26f4751fa1809a74cd0cdae2e0a741fb9d5
helooselassie <selassieheloo@gmail.com>
2025-01-15 19:14:27 +0000

    social media api

--- socialmedia/api/models.py
+++ socialmedia/api/models.py
@@ -1,9 +1,20 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 class User(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.URLField(blank=True, null=True)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_set",  # Avoid clash with default User.groups
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_permissions_set",  # Avoid clash with default User.user_permissions
+        blank=True
+    )
 
 
 class Post(models.Model):
