aba6d136dee9cb6a39dc3ee7b8c56c92dcc11d88
helooselassie <selassieheloo@gmail.com>
2025-01-16 03:03:40 +0000

    social media api

--- notifications/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from .views import NotificationListView
-
-urlpatterns = [
-    path('', views.NotificationList.as_view(), name='notifications'),
-    path('', NotificationListView.as_view(), name='notification_list'),
-]
