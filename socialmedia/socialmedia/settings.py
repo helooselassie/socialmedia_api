3a0cb7e288d122d3f3f3d6efe51fcc064197a708
helooselassie <selassieheloo@gmail.com>
2025-01-17 12:51:07 +0000

    social media api

--- socialmedia/socialmedia/settings.py
+++ socialmedia/socialmedia/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 #ALLOWED_HOSTS = ['helooselassie.pythonanywhere.com']
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'selassieheloos.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'selassieheloo.pythonanywhere.com']
 
 
 #import os
